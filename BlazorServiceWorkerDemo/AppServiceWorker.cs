4213cae958df338a698df9a58689b03a7b783558
Todd <todd@3dgamemarket.net>
2023-10-19 19:19:52 -0400

    Code cleanup (fixed too much code cleanup also)

--- BlazorServiceWorkerDemo/AppServiceWorker.cs
+++ BlazorServiceWorkerDemo/AppServiceWorker.cs
@@ -4,78 +4,78 @@ using SpawnDev.BlazorJS.WebWorkers;
 
 namespace BlazorServiceWorkerDemo
 {
-public class AppServiceWorker : ServiceWorkerManager
-{
-    public AppServiceWorker(BlazorJSRuntime js) : base(js)
+    public class AppServiceWorker : ServiceWorkerManager
     {
-
-    }
-
-    // called before any ServiceWorker events are handled
-    protected override async Task OnInitialized()
-    {
-        // This service will start in all scopes
-        // you can do initialization based on the scope that is running
-        Log("GlobalThisTypeName", JS.GlobalThisTypeName);
-        await Register();
-    }
-
-    protected override async Task ServiceWorker_OnInstallAsync(ExtendableEvent e)
-    {
-        Log($"ServiceWorker_OnInstallAsync");
-        _ = ServiceWorkerThis!.SkipWaiting();   // returned task can be ignored
-    }
-
-    protected override async Task ServiceWorker_OnActivateAsync(ExtendableEvent e)
-    {
-        Log($"ServiceWorker_OnActivateAsync");
-        await ServiceWorkerThis!.Clients.Claim();
-    }
-
-    protected override async Task<Response> ServiceWorker_OnFetchAsync(FetchEvent e)
-    {
-        Log($"ServiceWorker_OnFetchAsync", e.Request.Method, e.Request.Url);
-        Response ret;
-        try
+        public AppServiceWorker(BlazorJSRuntime js) : base(js)
         {
-            ret = await JS.Fetch(e.Request);
+
         }
-        catch (Exception ex)
+
+        // called before any ServiceWorker events are handled
+        protected override async Task OnInitialized()
         {
-            ret = new Response(ex.Message, new ResponseOptions { Status = 500, StatusText = ex.Message, Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } } });
-            Log($"ServiceWorker_OnFetchAsync failed: {ex.Message}");
+            // This service will start in all scopes
+            // you can do initialization based on the scope that is running
+            Log("GlobalThisTypeName", JS.GlobalThisTypeName);
+            await Register();
         }
-        return ret;
-    }
 
-    protected override async Task ServiceWorker_OnMessageAsync(ExtendableMessageEvent e)
-    {
-        Log($"ServiceWorker_OnMessageAsync");
-    }
+        protected override async Task ServiceWorker_OnInstallAsync(ExtendableEvent e)
+        {
+            Log($"ServiceWorker_OnInstallAsync");
+            _ = ServiceWorkerThis!.SkipWaiting();   // returned task can be ignored
+        }
 
-    protected override async Task ServiceWorker_OnPushAsync(PushEvent e)
-    {
-        Log($"ServiceWorker_OnPushAsync");
-    }
+        protected override async Task ServiceWorker_OnActivateAsync(ExtendableEvent e)
+        {
+            Log($"ServiceWorker_OnActivateAsync");
+            await ServiceWorkerThis!.Clients.Claim();
+        }
 
-    protected override void OnPushSubscriptionChange(Event e)
-    {
-        Log($"OnPushSubscriptionChange");
-    }
+        protected override async Task<Response> ServiceWorker_OnFetchAsync(FetchEvent e)
+        {
+            Log($"ServiceWorker_OnFetchAsync", e.Request.Method, e.Request.Url);
+            Response ret;
+            try
+            {
+                ret = await JS.Fetch(e.Request);
+            }
+            catch (Exception ex)
+            {
+                ret = new Response(ex.Message, new ResponseOptions { Status = 500, StatusText = ex.Message, Headers = new Dictionary<string, string> { { "Content-Type", "text/plain" } } });
+                Log($"ServiceWorker_OnFetchAsync failed: {ex.Message}");
+            }
+            return ret;
+        }
 
-    protected override async Task ServiceWorker_OnSyncAsync(SyncEvent e)
-    {
-        Log($"ServiceWorker_OnSyncAsync");
-    }
+        protected override async Task ServiceWorker_OnMessageAsync(ExtendableMessageEvent e)
+        {
+            Log($"ServiceWorker_OnMessageAsync");
+        }
 
-    protected override async Task ServiceWorker_OnNotificationCloseAsync(NotificationEvent e)
-    {
-        Log($"ServiceWorker_OnNotificationCloseAsync");
-    }
+        protected override async Task ServiceWorker_OnPushAsync(PushEvent e)
+        {
+            Log($"ServiceWorker_OnPushAsync");
+        }
 
-    protected override async Task ServiceWorker_OnNotificationClickAsync(NotificationEvent e)
-    {
-        Log($"ServiceWorker_OnNotificationClickAsync");
+        protected override void OnPushSubscriptionChange(Event e)
+        {
+            Log($"OnPushSubscriptionChange");
+        }
+
+        protected override async Task ServiceWorker_OnSyncAsync(SyncEvent e)
+        {
+            Log($"ServiceWorker_OnSyncAsync");
+        }
+
+        protected override async Task ServiceWorker_OnNotificationCloseAsync(NotificationEvent e)
+        {
+            Log($"ServiceWorker_OnNotificationCloseAsync");
+        }
+
+        protected override async Task ServiceWorker_OnNotificationClickAsync(NotificationEvent e)
+        {
+            Log($"ServiceWorker_OnNotificationClickAsync");
+        }
     }
-}
 }
--- BlazorServiceWorkerDemo/Program.cs
+++ BlazorServiceWorkerDemo/Program.cs
@@ -1,3 +1,5 @@
+using BlazorServiceWorkerDemo.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SpawnDev.BlazorJS;
 using SpawnDev.BlazorJS.WebWorkers;
@@ -9,13 +11,14 @@ namespace BlazorServiceWorkerDemo
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
             // SpawnDev.BlazorJS
             builder.Services.AddBlazorJSRuntime();
             // SpawnDev.BlazorJS.WebWorkers
             builder.Services.AddWebWorkerService();
             // Our ServiceWorker handler AppServiceWorker (inherits from ServiceWorkerManager)
             builder.Services.RegisterServiceWorker<AppServiceWorker>();
-            builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             // SpawnDev.BlazorJS startup (replaces RunAsync())
             await builder.Build().BlazorJSRunAsync();
         }
