f26170895235735a287919d9f28cc45795c43a6a
LostBeard <LostBeard@users.noreply.github.com>
2023-10-19 20:45:06 +0000

    Deploying to gh-pages from @ LostBeard/BlazorServiceWorkerDemo@13260802e224806ad6f90683a760ceb597530d6e 🚀

---
+++ _content/SpawnDev.BlazorJS.WebWorkers/spawndev.blazorjs.webworkers.js
@@ -0,0 +1,292 @@
+'use strict';
+
+// Todd Tanner
+// 2022 - 2023
+// SpawnDev.BlazorJS.WebWorkers
+// _content/SpawnDev.BlazorJS.WebWorkers/spawndev.blazorjs.webworkers.js
+// this script loads a fake window and DOM environment
+// and makes the Blazor WASM work in a DedicatedWorkerGlobalScope, a SharedWorkerGlobalScope and a ServiceWorkerGlobalScope
+
+var checkIfGlobalThis = function (it) {
+    return it && it.Math == Math && it;
+};
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/globalThis
+const globalThisObj =
+    // eslint-disable-next-line es/no-global-this -- safe
+    checkIfGlobalThis(typeof globalThis == 'object' && globalThis) ||
+    checkIfGlobalThis(typeof window == 'object' && window) ||
+    // eslint-disable-next-line no-restricted-globals -- safe
+    checkIfGlobalThis(typeof self == 'object' && self) ||
+    checkIfGlobalThis(typeof global == 'object' && global) ||
+    // eslint-disable-next-line no-new-func -- fallback
+    (function () { return this; })() || Function('return this')();
+
+const globalThisTypeName = globalThisObj.constructor.name;
+
+var disableHotReload = true;
+var verboseWebWorkers = location.search.indexOf('verbose=true') > -1;
+
+var consoleLog = function () {
+    if (!verboseWebWorkers) return;
+    console.log(...arguments);
+};
+
+consoleLog('spawndev.blazorjs.webworkers: started');
+consoleLog('location.href', location.href);
+
+// in some contexts, event handlers need to be added immediately and cannot wait for Blazor WASM's async startup
+// for example:
+// in a shared worker, the onconnect event handler needs to be set immediately to catch all invocations of the event
+// at this time, I ma not sure about fetch on service worker contexts. some debug code is being left until known.
+if (globalThisTypeName == 'SharedWorkerGlobalScope') {
+    // important for SharedWorker
+    // catch any incoming connetions that happen while .Net is loading
+    let _missedConnections = [];
+    globalThisObj.takeOverOnConnectEvent = function (newConnectFunction) {
+        var tmp = _missedConnections;
+        _missedConnections = [];
+        globalThisObj.onconnect = newConnectFunction;
+        return tmp;
+    }
+    globalThisObj.onconnect = function (e) {
+        _missedConnections.push(e.ports[0]);
+    };
+} else if (globalThisTypeName == 'ServiceWorkerGlobalScope') {
+    let holdEvents = true;
+    let missedServiceWorkerEventts = [];
+    function handleMissedEvent(e) {
+        if (!holdEvents) return;
+        console.log('ServiceWorker missed event:', e.type, e);
+        if (e.waitUntil) {
+            var waitUntilPromise = new Promise(function (resolve, reject) {
+                e.waitResolve = resolve;
+                e.waitReject = reject;
+            });
+            e.waitUntil(waitUntilPromise);
+        }
+        if (e.respondWith) {
+            var responsePromise = new Promise(function (resolve, reject) {
+                e.responseResolve = resolve;
+                e.responseReject = reject;
+            });
+            e.respondWith(responsePromise);
+        }
+        missedServiceWorkerEventts.push(e);
+    }
+    self.addEventListener('activate', handleMissedEvent);
+    self.addEventListener('fetch', handleMissedEvent);
+    self.addEventListener('install', handleMissedEvent);
+    self.addEventListener('message', handleMissedEvent);
+    self.addEventListener('notificationclick', handleMissedEvent);
+    self.addEventListener('notificationclose', handleMissedEvent);
+    self.addEventListener('push', handleMissedEvent);
+    self.addEventListener('pushsubscriptionchange', handleMissedEvent);
+    self.addEventListener('sync', handleMissedEvent);
+    globalThisObj.GetMissedServiceWorkerEvents = function () {
+        holdEvents = false;
+        var ret = missedServiceWorkerEventts;
+        missedServiceWorkerEventts = [];
+        return ret;
+    };
+}
+
+// location.href is this script
+// - location.href == 'https://localhost:7191/_content/SpawnDev.BlazorJS.WebWorkers/spawndev.blazorjs.webworkers.js?verbose=false'
+// or a service worker script
+// - location.href == 'https://localhost:7191/service-worker.js'
+// if documentBaseURIIsModified == true, 
+// - fetch will be replaced with one that uses the modified documentBaseURI as its base path for relative path fetches as is expected in Blazor WASM apps
+var documentBaseURIIsModified = false;
+var documentBaseURI = (function () {
+    var uri = new URL(`./`, location.href);
+    if (uri.pathname.includes('_content/')) {
+        documentBaseURIIsModified = true;
+        var subpath = uri.pathname.substring(0, uri.pathname.indexOf('_content/'));
+        return new URL(subpath, location.href).toString();
+    }
+    return uri.toString();
+})();
+consoleLog('documentBaseURI', documentBaseURI);
+
+const webWorkersContent = new URL(`_content/SpawnDev.BlazorJS.WebWorkers/`, documentBaseURI).toString();
+consoleLog('spawndev.blazorjs.webworkers: loading fake window environment');
+// txml - xml parser
+// https://github.com/TobiasNickel/tXml
+importScripts(new URL('txml.min.js', webWorkersContent).toString());
+// faux DOM and document environment
+//importScripts('spawndev.blazorjs.webworkers.faux-env.js');
+importScripts(new URL('spawndev.blazorjs.webworkers.faux-env.js', webWorkersContent).toString());
+// faux dom and window environment has been created (currently empty)
+// set document.baseURI to the apps basePath (which is relative to this scripts path)
+document.baseURI = documentBaseURI;
+
+if (disableHotReload) {
+    consoleLog('disabling hot reload on this thread');
+    const scriptInjectedSentinel = '_dotnet_watch_ws_injected'
+    globalThisObj[scriptInjectedSentinel] = true
+}
+
+async function hasDynamicImport() {
+    // ServiceWorkers have issues with dynamic imports even if detection says it is supported 
+    if (globalThisTypeName == 'ServiceWorkerGlobalScope') {
+        return false;
+    }
+    try {
+        await import('data:text/javascript;base64,Cg==');
+        return true;
+    } catch (e) {
+        return false;
+    }
+}
+
+var initWebWorkerBlazor = async function () {
+    var dynamicImportSupported = await hasDynamicImport();
+    // Firefox, and possibly some other browsers, do not support dynamic module import (import) in workers.
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=1540913
+    // Some scripts will have to be patched on the fly if import is not supported.
+    if (!dynamicImportSupported) {
+        consoleLog("import is not supported. A workaround will be used.");
+    } else {
+        consoleLog('import is supported.');
+    }
+    // patch globalThisObj.fetch to use document.baseURI for the relative path base path
+    if (documentBaseURIIsModified) {
+        let fetchOrig = globalThisObj.fetch;
+        globalThisObj.fetch = function (resource, options) {
+            consoleLog("webWorkersFetch", typeof resource, resource);
+            if (typeof resource === 'string') {
+                // resource is a string
+                const newUrl = new URL(resource, document.baseURI);
+                return fetchOrig(newUrl, options);
+            } else {
+                // resource is a Request object
+                // currently not modified. could cause issues if a relative path was used to create the Request object.
+                return fetchOrig(resource, options);
+            }
+        };
+    }
+    // fetch getText method
+    async function getText(href) {
+        var response = await fetch(new URL(href, document.baseURI));
+        return await response.text();
+    }
+    // Get index.html and parse it (for scripts, etc)
+    function parseHTML(html) {
+        var dom = txml.parse(html);
+        function addParentNode(children, parentNode) {
+            if (parentNode) parentNode.text = '';
+            for (let i = 0; i < children.length; i++) {
+                let child = children[i];
+                if (typeof child === 'string') {
+                    if (parentNode) parentNode.text = child;
+                    children.splice(i, 1);
+                    i--;
+                } else if (child) {
+                    child.parentNode = parentNode;
+                    if (child.children) {
+                        addParentNode(child.children, child);
+                    }
+                }
+            }
+        }
+        addParentNode(dom);
+        return dom;
+    }
+    var dom = parseHTML(await getText('index.html'));
+    var indexHtmlScripts = txml.filter(dom, o => o.tagName && o.tagName.toLowerCase() === 'script');
+    globalThisObj.importOverride = async function (src) {
+        consoleLog('importOverride', src);
+        var jsStr = await getText(src);
+        jsStr = fixModuleScript(jsStr, src);
+        let fn = new Function(jsStr);
+        var ret = fn.apply(createProxiedObject(globalThisObj), []);
+        if (!ret) ret = createProxiedObject({});
+        return ret;
+    }
+    // this method fixes 'dynamic import scripts' to work in an environment that does not support 'dynamic import scripts'
+    // it is designed for and tested agaisnt the Blazor WASM runtime.
+    // it may not work on other modules
+    function fixModuleScript(jsStr, src) {
+        // handle things that are automatically handled by import
+        src = new URL(src, document.baseURI).toString();
+        var scriptUrl = JSON.stringify(src);
+        consoleLog('fixModuleScript.scriptUrl', src, scriptUrl);
+        // fix import.meta.url - The full URL to the module
+        jsStr = jsStr.replace(/\bimport\.meta\.url\b/g, scriptUrl);
+        // import.meta
+        jsStr = jsStr.replace(/\bimport\.meta\b/g, `{ url: ${scriptUrl} }`);
+        // import
+        jsStr = jsStr.replace(/\bimport\(/g, 'importOverride(');
+        // export
+        // https://www.geeksforgeeks.org/what-is-export-default-in-javascript/
+        // handle exports from
+        // lib modules
+        // Ex(_content/SpawnDev.BlazorJS/SpawnDev.BlazorJS.lib.module.js)
+        // export function beforeStart(options, extensions) {
+        // export function afterStarted(options, extensions) {
+        var exportPatt = /\bexport[ \t]+function[ \t]+([^ \t(]+)/g;
+        jsStr = jsStr.replace(exportPatt, '_exportsOverride.$1 = function $1');
+        // To match: _framework/blazor-hotreload.
+        // export async function receiveHotReloadAsync() {
+        exportPatt = /\bexport[ \t]+async[ \t]+function[ \t]+([^ \t(]+)/g;
+        jsStr = jsStr.replace(exportPatt, '_exportsOverride.$1 = async function $1');
+        // handle exports from
+        // dotnet.7.0.0.amub20uvka.js
+        // export default createDotnetRuntime
+        exportPatt = /\bexport[ \t]+default[ \t]+([^ \t;]+)/g;
+        jsStr = jsStr.replace(exportPatt, '_exportsOverride.default = $1');
+        // export{Be as default,Fe as dotnet,We as exit};
+        // below changes the above line to the below line changing the 'VAR as KEY' to 'KEY:VAR'
+        // export{default:Be,dotnet:Fe,exit:We};
+        exportPatt = /([a-zA-Z0-9]+)\s+as\s+([a-zA-Z0-9]+)/g;
+        jsStr = jsStr.replace(exportPatt, '$2:$1');
+        // export { dotnet, exit, INTERNAL };
+        exportPatt = /\bexport\b[ \t]*(\{[^}]+\})/g;
+        jsStr = jsStr.replace(exportPatt, '_exportsOverride = Object.assign(_exportsOverride, $1)');
+        var modulize = `let _exportsOverride = {}; ${jsStr}; return _exportsOverride;`;
+        return modulize;
+    }
+    async function initializeBlazor() {
+        // setup standard document
+        var htmlEl = document.appendChild(document.createElement('html'));
+        var headEl = htmlEl.appendChild(document.createElement('head'));
+        var bodyEl = htmlEl.appendChild(document.createElement('body'));
+        // add blazor specific stuff
+        // <div id="app">
+        var appDiv = bodyEl.appendChild(document.createElement('div'));
+        appDiv.setAttribute('id', 'app');
+        // <div id="blazor-error-ui">
+        var errorDiv = bodyEl.appendChild(document.createElement('div'));
+        errorDiv.setAttribute('id', 'blazor-error-ui');
+        // <script src="_framework/blazor.webassembly.js"></script>
+        // load webworker-enabled scripts in order found in index.html (and _framework/blazor.webassembly.js)
+        for (var indexHtmlScript of indexHtmlScripts) {
+            let src = indexHtmlScript.attributes.src;
+            let isBlazorWebAssemblyJS = src && src.includes('_framework/blazor.webassembly.js');
+            let isWebWorkerEnabled = typeof indexHtmlScript.attributes['webworker-enabled'] !== 'undefined' && indexHtmlScript.attributes['webworker-enabled'] !== 'false';
+            if (!isBlazorWebAssemblyJS && !isWebWorkerEnabled) continue;
+            let scriptEl = document.createElement('script');
+            if (indexHtmlScript.parentNode && indexHtmlScript.parentNode.tagName.toLowerCase() === 'head') {
+                headEl.appendChild(scriptEl);
+            } else {
+                bodyEl.appendChild(scriptEl);
+            }
+            for (var attr in indexHtmlScript.attributes) {
+                let attrValue = indexHtmlScript.attributes[attr];
+                scriptEl.setAttribute(attr, attrValue);
+            }
+            if (indexHtmlScript.text) scriptEl.text = indexHtmlScript.text;
+            if (isBlazorWebAssemblyJS && !dynamicImportSupported) {
+                // load script text so we can do some on-the-fly patching to fix compatibility with WebWorkers
+                let jsStr = await getText(src);
+                // fix dynamic imports
+                scriptEl.text = fixModuleScript(jsStr, src);
+            }
+        }
+        // init document
+        document.initDocument();
+    }
+    await initializeBlazor();
+};
+initWebWorkerBlazor();
---
+++ service-worker-assets.js
@@ -0,0 +1,265 @@
+﻿self.assetsManifest = {
+  "assets": [
+    {
+      "hash": "sha256-yis2dNdJ6e9R472mcuwUOsxBPCQX3snSMmHAL3zu2D8=",
+      "url": "css\/app.css"
+    },
+    {
+      "hash": "sha256-z8OR40MowJ8GgK6P89Y+hiJK5+cclzFHzLhFQLL92bg=",
+      "url": "css\/bootstrap\/bootstrap.min.css"
+    },
+    {
+      "hash": "sha256-gBwg2tmA0Ci2u54gMF1jNCVku6vznarkLS6D76htNNQ=",
+      "url": "css\/bootstrap\/bootstrap.min.css.map"
+    },
+    {
+      "hash": "sha256-4mWsDy3aHl36ZbGt8zByK7Pvd4kRUoNgTYzRnwmPHwg=",
+      "url": "favicon.png"
+    },
+    {
+      "hash": "sha256-DbpQaq68ZSb5IoPosBErM1QWBfsbTxpJqhU0REi6wP4=",
+      "url": "icon-192.png"
+    },
+    {
+      "hash": "sha256-oEo6d+KqX5fjxTiZk\/w9NB3Mi0+ycS5yLwCKwr4IkbA=",
+      "url": "icon-512.png"
+    },
+    {
+      "hash": "sha256-fCh2tF36Pc4h4lbXyEOou1yccSt7QDC\/lnFApl9VbyI=",
+      "url": "index.html"
+    },
+    {
+      "hash": "sha256-zlX9nMmwoZTxDyFuxfm+G4s7htXWZ1h83lwtTZdgLxk=",
+      "url": "manifest.webmanifest"
+    },
+    {
+      "hash": "sha256-paIeuWnSzjeLGITWkk4p3rcejUsZWCJGPeOK9UhWXtg=",
+      "url": "sample-data\/weather.json"
+    },
+    {
+      "hash": "sha256-IEU1twEQUygzHnol9ihcgPT0a6vL1X8J7FmFMm0AKdM=",
+      "url": "_framework\/blazor.webassembly.js"
+    },
+    {
+      "hash": "sha256-u+8Xakm+L99EG4GRNQDn9VDkLYMUZnxgamuKuiLhZ9c=",
+      "url": "_framework\/dotnet.js"
+    },
+    {
+      "hash": "sha256-giy\/yLhvB9hsa1DCWVWnpxpuf6kKvlYFh5rifY3mpQg=",
+      "url": "_framework\/dotnet.runtime.8.0.0-rc.2.23479.6.lf65z6envp.js"
+    },
+    {
+      "hash": "sha256-SZLtQnRc0JkwqHab0VUVP7T3uBPSeYzxzDnpxPpUnHk=",
+      "url": "_framework\/icudt_CJK.dat"
+    },
+    {
+      "hash": "sha256-8fItetYY8kQ0ww6oxwTLiT3oXlBwHKumbeP2pRF4yTc=",
+      "url": "_framework\/icudt_EFIGS.dat"
+    },
+    {
+      "hash": "sha256-L7sV7NEYP37\/Qr2FPCePo5cJqRgTXRwGHuwF5Q+0Nfs=",
+      "url": "_framework\/icudt_no_CJK.dat"
+    },
+    {
+      "hash": "sha256-QGQ6rWqYG04sBWCHn2m247Ff1YawlPwZuPx4AuvyVCo=",
+      "url": "_framework\/blazor.boot.json"
+    },
+    {
+      "hash": "sha256-rwU1p2UF14GBwzwb2M7PDR8nKfhv+d\/GftDEZ+xx1Z0=",
+      "url": "_framework\/dotnet.native.8.0.0-rc.2.23479.6.chf54js2fo.js"
+    },
+    {
+      "hash": "sha256-celamDTuQXPCgm4mj0cLuNpUZLPYntUJIyGHZv6INPs=",
+      "url": "_framework\/dotnet.native.wasm"
+    },
+    {
+      "hash": "sha256-ioTb0kBkbWP\/WR\/UqS6pPziLF0BZiTNo4Nac1U7MMQw=",
+      "url": "_framework\/BlazorServiceWorkerDemo.wasm"
+    },
+    {
+      "hash": "sha256-1Wi406FH3OSeCn5SAwxoWtIG70ccP4CYIMdKGXy\/faw=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.wasm"
+    },
+    {
+      "hash": "sha256-kJK45VZutXrFBO3GBjvBTTqKml+zaDXHTLAvjCL2\/dc=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.Web.wasm"
+    },
+    {
+      "hash": "sha256-+DIyvDameI68zZEg9ovK068ylRZ1O0AiCOxWAXPzW60=",
+      "url": "_framework\/Microsoft.AspNetCore.Components.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-nRWpooD3YXX9R3VQCRqqsi0z5U54eDkiho7m8cpM2O0=",
+      "url": "_framework\/Microsoft.CSharp.wasm"
+    },
+    {
+      "hash": "sha256-Jpr+z5efR8wQJ7BFfeaSSLwlws0KdX9DBWj5iUUEBZ0=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-fly0SpczcONrTRPU+wlmpWuWLHopTsPprE6CvUbLsjE=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.Json.wasm"
+    },
+    {
+      "hash": "sha256-gG2TpIyuOZ+ZGLGD7HpAnve3LX1FUYFUfCYj0WrL\/iM=",
+      "url": "_framework\/Microsoft.Extensions.Configuration.wasm"
+    },
+    {
+      "hash": "sha256-1lOQBSD1KQRMVv\/XLDngw5UMdpRGhST5\/UloqLB9twg=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-gyhn3ex290mY2noOq0EkX6\/zITuVw+l5H8a2e6WQu\/A=",
+      "url": "_framework\/Microsoft.Extensions.DependencyInjection.wasm"
+    },
+    {
+      "hash": "sha256-wzB6y45jsPs2+Hdn0sCfz2rZ2G+oHv5DfctnKKeQELE=",
+      "url": "_framework\/Microsoft.Extensions.Logging.Abstractions.wasm"
+    },
+    {
+      "hash": "sha256-dgKvXywOaPXClparUTVY2FqSbgG+r\/o4zIDhtspDdfk=",
+      "url": "_framework\/Microsoft.Extensions.Logging.wasm"
+    },
+    {
+      "hash": "sha256-6wMoDCgmaY4DwL6WcLpTawcUyyNk5WyJy7sks0nMYEc=",
+      "url": "_framework\/Microsoft.Extensions.Options.wasm"
+    },
+    {
+      "hash": "sha256-whIW3a0cFyCLe2u252Nlm1jSHLZFkrAeCHfDaP5XsS8=",
+      "url": "_framework\/Microsoft.Extensions.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-NlMMHDRXT41Dv6BZ0e3iggGPWYP571czhqkGq6wZqGI=",
+      "url": "_framework\/Microsoft.JSInterop.wasm"
+    },
+    {
+      "hash": "sha256-VppfYneK9xqADXMOBaq9+3Zl\/REDyPKz2UEm8\/QBi8M=",
+      "url": "_framework\/Microsoft.JSInterop.WebAssembly.wasm"
+    },
+    {
+      "hash": "sha256-wkoxGfx3ExnU8J54AJ4Vxd7f74o1MtWcaq4lzjzrQdQ=",
+      "url": "_framework\/SpawnDev.BlazorJS.wasm"
+    },
+    {
+      "hash": "sha256-c3YUQJX0QrYco\/R2EXXk7aOcyewOhb4q2M0wJZ3p95U=",
+      "url": "_framework\/SpawnDev.BlazorJS.WebWorkers.wasm"
+    },
+    {
+      "hash": "sha256-Nz2rklRRkGLzdga9HIZDDi44QEKTPzWifFj8vfygY8M=",
+      "url": "_framework\/System.Collections.Concurrent.wasm"
+    },
+    {
+      "hash": "sha256-Sdh7FS5tYMet9WE7DbdkbCxo0oGaa0vQ0i107M7LYVM=",
+      "url": "_framework\/System.Collections.Specialized.wasm"
+    },
+    {
+      "hash": "sha256-GQICCVlrWhq+a\/UFdqoCVV2Z99p3i+sA1whuQ\/aNKd4=",
+      "url": "_framework\/System.Collections.wasm"
+    },
+    {
+      "hash": "sha256-V9DbGl95R3W4XATA\/S28mTHULqEzXRHNGXxJxJcjQ5k=",
+      "url": "_framework\/System.ComponentModel.wasm"
+    },
+    {
+      "hash": "sha256-GoGErb7BHQAmvGwTQH+6uQ5kgX9QfwsPbV219BkafVY=",
+      "url": "_framework\/System.Console.wasm"
+    },
+    {
+      "hash": "sha256-mq55j0ep78gmmMW\/CeXY2bU4u2NFmCj0oXTqcNZviS8=",
+      "url": "_framework\/System.Diagnostics.DiagnosticSource.wasm"
+    },
+    {
+      "hash": "sha256-QUZtoflYAYSeQBHgJ0hejBbKd0dQgZ5CI6H97yjVXq0=",
+      "url": "_framework\/System.Linq.Expressions.wasm"
+    },
+    {
+      "hash": "sha256-mu0cDtVjJRkKWOsAYQT8FiZE7k0LdX2wuzZ\/e8vNxVI=",
+      "url": "_framework\/System.Linq.wasm"
+    },
+    {
+      "hash": "sha256-x0iKTcBvaUX6NJkBbBFSwO6lCT7fG8aeJC7bjIO3WYU=",
+      "url": "_framework\/System.Memory.wasm"
+    },
+    {
+      "hash": "sha256-Ftpp9qh8hCSBm2mKhYHfXDSrsezePFM6Ogm6NX6H\/ro=",
+      "url": "_framework\/System.Net.Http.Json.wasm"
+    },
+    {
+      "hash": "sha256-uposT6ffo0eyI+evUNZSfmf+pNhN87UzU9SRuw5jar8=",
+      "url": "_framework\/System.Net.Http.wasm"
+    },
+    {
+      "hash": "sha256-fmZzddXMY4mBna15b1oGyxwsGKLwn5R6iP6rUQURZxM=",
+      "url": "_framework\/System.Net.Primitives.wasm"
+    },
+    {
+      "hash": "sha256-yU7BC37Ieq6JrTY04vTZSqAv2f1NEzJUbSIhvkvcRWE=",
+      "url": "_framework\/System.ObjectModel.wasm"
+    },
+    {
+      "hash": "sha256-ZzDgXEzQOyjl97CevftbdUo1XDKjGipUl9f+Wc\/e9pA=",
+      "url": "_framework\/System.Private.CoreLib.wasm"
+    },
+    {
+      "hash": "sha256-7EeKerZC2uON5vHgK6XsdnNnk8Z7XvwFQI0+kZBgP+g=",
+      "url": "_framework\/System.Private.Uri.wasm"
+    },
+    {
+      "hash": "sha256-Pn8t\/J6P3bXvg9nWbLCNWuG6oP41T65nlNGGTK8+s1k=",
+      "url": "_framework\/System.Reflection.DispatchProxy.wasm"
+    },
+    {
+      "hash": "sha256-4tH1Mo2Lt8RRGpCdDrk3cheyuPYq\/m\/PiWuSODopEks=",
+      "url": "_framework\/System.Runtime.InteropServices.JavaScript.wasm"
+    },
+    {
+      "hash": "sha256-IUWG4iSxTODJPOaqD3APvGuUqRceAu6INda7mRaFgAw=",
+      "url": "_framework\/System.Runtime.wasm"
+    },
+    {
+      "hash": "sha256-KCXz6I7ccIUPe0U3Dg\/qVYXuAeSsHTRNXiNZHWX8gag=",
+      "url": "_framework\/System.Text.Encodings.Web.wasm"
+    },
+    {
+      "hash": "sha256-XxFDsUmHlv+waMmAugXUC6C5wbzOr62g1mqDg4uvPI0=",
+      "url": "_framework\/System.Text.Json.wasm"
+    },
+    {
+      "hash": "sha256-9CesxvCn4+BtVVcMDXrADBzkoGOik+1FOIjcVXVZaEY=",
+      "url": "_framework\/System.Text.RegularExpressions.wasm"
+    },
+    {
+      "hash": "sha256-ec8T0KMWjMYAeS2zoCmcSr8esQLzxl8fzGN9pc+m8eU=",
+      "url": "_framework\/System.Threading.wasm"
+    },
+    {
+      "hash": "sha256-MxdT\/79xpMIu66VECPXI8bunOreOYxw5o17qNLBhzfA=",
+      "url": "_framework\/System.Web.HttpUtility.wasm"
+    },
+    {
+      "hash": "sha256-P6msWBj1eKLlYcGlJQ\/y8K+pqVnOOCKqvAepZTlLpp0=",
+      "url": "BlazorServiceWorkerDemo.styles.css"
+    },
+    {
+      "hash": "sha256-rqJ7D3sy8sixnTawpyRXd8KLz14SSpGRBpn14OTivFI=",
+      "url": "_content\/SpawnDev.BlazorJS.WebWorkers\/spawndev.blazorjs.webworkers.faux-env.js"
+    },
+    {
+      "hash": "sha256-ldQpxNrRoBtDV3AEmkpa0VGAzG58anAUSG0JQmAPWM4=",
+      "url": "_content\/SpawnDev.BlazorJS.WebWorkers\/spawndev.blazorjs.webworkers.js"
+    },
+    {
+      "hash": "sha256-4qJxYL1lIjQi7cbsyvsb3ODmaBRid2eS0rIMqbdbeLs=",
+      "url": "_content\/SpawnDev.BlazorJS.WebWorkers\/txml.min.js"
+    },
+    {
+      "hash": "sha256-BNHp0TVGVoaozHVsFp1hUzmi+xROdLaoo\/S1NhWMerk=",
+      "url": "_content\/SpawnDev.BlazorJS.WebWorkers\/SpawnDev.BlazorJS.WebWorkers.lib.module.js"
+    },
+    {
+      "hash": "sha256-ztXMPCO+zRbDUQyb7pvMRcXzvF0H7TMNb69oeeyhtfk=",
+      "url": "_content\/SpawnDev.BlazorJS\/SpawnDev.BlazorJS.lib.module.js"
+    }
+  ],
+  "version": "GwwZqMIC"
+};
